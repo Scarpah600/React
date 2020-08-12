a90bd2415c67f69b35a0bbf64f13c4fa6cf4f184
Gustavo Scarpin <40478729+Scarpah600@users.noreply.github.com>
2020-08-12 17:50:16 -0300

    Contador 2

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,5 +1,9 @@
 import React, {Component} from 'react'
 import "./Contador.css"
+import Display from './Display';
+import Botoes from './Botoes';
+import PassoForm from './PassoForm'
+
 
 export default class Contador extends Component {
 
@@ -13,6 +17,7 @@ export default class Contador extends Component {
         constructor(props){
        super(props)
        this.inc = this.inc.bind(this)
+       <PassoForm passo={this.state.passo} /> //Direta
    }
     }
 
@@ -38,9 +43,9 @@ export default class Contador extends Component {
     });
   }
 
-  setPasso = (e) =>{
+  setPasso = (novoPasso) =>{
       this.setState({
-          passo: +e.target.value,
+          passo: novoPasso,
       });
   }
 
@@ -49,15 +54,9 @@ export default class Contador extends Component {
         <div className="Contador">
             <h2>Contador</h2>
                 <h3>{this.state.numero}</h3>
-                <div>
-                    <label htmlFor="passoInput">Passo:</label>
-                    <input id="passoInput" 
-                    type="number" 
-                    value={this.state.passo}
-                    onChange={this.setPasso}/>
-                </div>
-             <button onClick={this.inc}>+</button>
-             <button onClick={this.dec}>-</button>
+                <Display numero={this.state.numero}></Display>
+                <PassoForm passo={this.state.passo}  setPasso={this.setPasso}/> 
+                <Botoes setInc={this.inc} setDec={this.dec}></Botoes>
         </div>
         );
     }
